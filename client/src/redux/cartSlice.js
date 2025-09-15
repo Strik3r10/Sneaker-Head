9f89ed445f2c70efb3f1534a45b5b3efd69da29d
Owais Farooqui <owais@example.com>
2025-09-15 07:30:05 +0530

    minor fixes in routes

--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -2,3 +2,5 @@
 // Improve frontend responsiveness with Tailwind
 // Add Product model and schema
 console.log('Commit #2: Add Product model and schema');
+// minor fixes in routes
+console.log('Commit #3: minor fixes in routes');
