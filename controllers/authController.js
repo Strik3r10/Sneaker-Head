ffa0e6b7a73e58c859544727d031db9a233d158b
Owais Farooqui <owais@example.com>
2025-09-22 00:30:19 +0530

    environment vars added

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,3 +5,5 @@ console.log('Commit #3: Implement CRUD routes for products');
 console.log('Commit #19: Fix CORS issues');
 // some db config updates
 console.log('Commit #2: some db config updates');
+// environment vars added
+console.log('Commit #17: environment vars added');
