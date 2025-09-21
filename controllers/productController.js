6635b680f7609bd1b5d393355470b3053dc76284
Owais Farooqui <owais@example.com>
2025-09-21 12:30:16 +0530

    hooked up API calls

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,3 +4,5 @@
 console.log('Commit #7: Add Navbar and Footer components');
 // Final testing and deployment setup
 console.log('Commit #24: Final testing and deployment setup');
+// hooked up API calls
+console.log('Commit #13: hooked up API calls');
