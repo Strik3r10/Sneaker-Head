c1eba41be34090a39a76b474596c169253f31961
Owais Farooqui <owais@example.com>
2025-10-20 09:27:56 +0530

    Add spinners and toast notifications

--- config/db.js
+++ config/db.js
@@ -5,3 +5,5 @@ console.log('Commit #12: Add Add-to-Cart functionality (frontend)');
 console.log('Commit #13: Setup Redux for global cart management');
 // Add bcrypt password hashing
 console.log('Commit #16: Add bcrypt password hashing');
+// Add spinners and toast notifications
+console.log('Commit #21: Add spinners and toast notifications');
