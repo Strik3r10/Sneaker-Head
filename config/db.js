0f7e28c04595b5be092b5b252ec1db92934e2be8
Owais Farooqui <owais@example.com>
2025-11-04 20:27:51 +0530

    Add bcrypt password hashing

--- config/db.js
+++ config/db.js
@@ -3,3 +3,5 @@
 console.log('Commit #12: Add Add-to-Cart functionality (frontend)');
 // Setup Redux for global cart management
 console.log('Commit #13: Setup Redux for global cart management');
+// Add bcrypt password hashing
+console.log('Commit #16: Add bcrypt password hashing');
