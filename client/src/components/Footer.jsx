8db98e213b9cab231f3350a1dd8834c1eeb70093
Owais Farooqui <owais@example.com>
2025-11-10 05:30:12 +0530

    added some utils

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,3 +3,5 @@
 // Add bcrypt password hashing
 // Improve frontend responsiveness with Tailwind
 console.log('Commit #20: Improve frontend responsiveness with Tailwind');
+// added some utils
+console.log('Commit #10: added some utils');
