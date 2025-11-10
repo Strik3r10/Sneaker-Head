f8a6f672b5e003ffe76420470b0981470064c486
Owais Farooqui <owais@example.com>
2025-11-10 18:27:47 +0530

    Setup Redux for global cart management

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
 // Refactor backend structure
 // Add Add-to-Cart functionality (frontend)
 console.log('Commit #12: Add Add-to-Cart functionality (frontend)');
+// Setup Redux for global cart management
+console.log('Commit #13: Setup Redux for global cart management');
